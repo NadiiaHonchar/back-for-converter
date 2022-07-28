aa3023ffc1d450b95b0e67cea2af7a1b2669c0b8
NadiiaHonchar <17nmg82@gmail.com>
2022-07-28 21:12:24 +0300

    add DB connect

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose")
+require("dotenv").config();
 
+const {DB_HOST} = process.env;
 const app = express();
+mongoose.connect(DB_HOST).then(()=>console.log("Database success connect")).catch(error=>console.log(error.message));
 
 app.listen(4000,()=>console.log("server running"))
